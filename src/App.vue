9d059ecd1c0f2a6044886c1734209efb0a04ff78
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-28 21:02:24 +0200

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <Home />
+  <router-view />
 </template>
 
 <script>
-import Home from './views/Home.vue';
+import SignIn from './views/SignIn.vue';
 
 export default {
   name: 'App',
   components: {
-    Home,
+    SignIn,
   },
 };
 </script>
