dff37a7f97512d1eec3f462aa978b49dc3b6aeee
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-30 20:58:03 +0200

    header component

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <Logo />
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Logo from './Logo.vue';
+import Navbar from './Navbar.vue';
+
+export default {
+  name: 'HeaderBar',
+  components: { Logo, Navbar },
+};
+</script>
+
+<style scoped>
+.header {
+  background: lightblue;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
