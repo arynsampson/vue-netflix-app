2d02d53d51c0ceca117bfd1672011fb5b9f5d69a
Aryn Sampson <arynsampson0704@gmail.com>
2022-08-30 20:59:02 +0200

    watchlist component

---
+++ src/views/WatchList.vue
@@ -0,0 +1,15 @@
+<template>
+  <HeaderBar />
+  <p>This is your watchlist</p>
+</template>
+
+<script>
+import HeaderBar from '../components/HeaderBar.vue';
+
+export default {
+  name: 'WatchList',
+  components: { HeaderBar },
+};
+</script>
+
+<style></style>
