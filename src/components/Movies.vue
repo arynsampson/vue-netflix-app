5e133eb534b66306f8c2c7f40aebde32cd5c5c42
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-01 20:44:07 +0200

    added display watchlist condition

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="home-content container">
-    <div v-for="movie in moviesComingSoon" :key="movie.id">
-      <Movie :item="movie" />
+  <div class="home-content content-container">
+    <div v-if="!watchListPage" class="carousel-wrapper">
+      <Carousel :movies="moviesArr" />
     </div>
-    <div>
-      <p>This is the break</p>
+    <div v-if="!watchListPage" v-for="movie in moviesAvailNow" :key="movie.id">
+      <Movie :item="movie" @updateMovieWatchListVal="addToWatchList" />
     </div>
-    <div v-for="movie in moviesAvailNow" :key="movie.id">
-      <Movie :item="movie" />
+    <div v-if="watchListPage" v-for="movie in watchListMovies" :key="movie.id">
+      <Movie
+        :watchListItem="watchListPage"
+        :item="movie"
+        @updateMovieWatchListVal="addToWatchList"
+      />
     </div>
   </div>
 </template>
@@ -15,33 +19,54 @@
 <script>
 import * as movies from '../mocks/data.json';
 import Movie from './Movie.vue';
+import Carousel from './Carousel.vue';
 
 export default {
   name: 'Movies',
   components: {
     Movie,
+    Carousel,
   },
+  props: ['watchListPageBool'],
   data() {
     return {
       moviesArr: [],
+      watchListPage: false,
     };
+  },
+  methods: {
+    addToWatchList(item) {
+      item.watchList = !item.watchList;
+    },
   },
   mounted() {
     this.moviesArr = movies.movies;
+
+    if (this.$props.watchListPageBool) {
+      this.watchListPage = true;
+    }
   },
   computed: {
-    moviesComingSoon() {
-      return this.moviesArr.filter((movie) => movie.comingSoon);
-    },
     moviesAvailNow() {
       return this.moviesArr.filter((movie) => !movie.comingSoon);
+    },
+    watchListMovies() {
+      return this.moviesArr.filter((movie) => movie.watchList);
+    },
+  },
+  watch: {
+    moviesArr: {
+      handler() {
+        localStorage.setItem('movies', JSON.stringify(this.moviesArr));
+      },
+      deep: true,
     },
   },
 };
 </script>
 
 <style scoped>
-.container {
+.content-container {
   max-width: 450px;
   display: flex;
   flex-direction: column;
@@ -53,29 +78,31 @@ export default {
 }
 
 @media screen and (min-width: 700px) {
-  .container {
+  .content-container {
     max-width: 600px;
     display: grid;
     grid-template-columns: 1fr 1fr;
     justify-items: center;
   }
+
+  .carousel-wrapper {
+    width: 100%;
+    grid-column: 1 / -1;
+  }
 }
 
 @media screen and (min-width: 1000px) {
-  .container {
+  .content-container {
     max-width: 950px;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    justify-items: center;
   }
 }
 
 @media screen and (min-width: 1400px) {
-  .container {
+  .content-container {
     max-width: 1200px;
-    display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
-    justify-items: center;
   }
 }
 </style>
