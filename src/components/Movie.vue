5078bdd77ec0e7e96dc6211bd82076c58c44fffe
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-01 20:42:43 +0200

    added add-to-wishlist func

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -3,14 +3,40 @@
     <div class="movie-thumbnail-wrapper">
       <img :src="item.thumbnail" :alt="item.name" class="movie-thumbnail" />
     </div>
-    <div class="overlay"></div>
+    <div class="overlay">
+      <div class="movie-content">
+        <p class="movie-title">{{ item.name }}</p>
+
+        <p
+          v-if="!watchListItem"
+          class="add-icon icon"
+          @click="$emit('updateMovieWatchListVal', item)"
+        >
+          Add
+        </p>
+
+        <p
+          v-if="watchListItem"
+          class="remove-icon icon"
+          @click="$emit('updateMovieWatchListVal', item)"
+        >
+          Remove
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Movie',
-  props: ['item'],
+  props: ['item', 'watchListItem'],
+  emits: ['updateMovieWatchListVal'],
+  methods: {
+    addToWishlist(item) {
+      item.watchList = !item.watchList;
+    },
+  },
 };
 </script>
 
@@ -35,8 +61,6 @@ export default {
 
 .overlay {
   position: absolute;
-  background: black;
-  opacity: 0;
   top: 0;
   left: 0;
   right: 0;
@@ -48,6 +72,33 @@ export default {
 }
 
 .overlay:hover {
-  opacity: 0.5;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.overlay:hover .movie-content {
+  display: block;
+}
+
+.movie-content {
+  display: none;
+}
+
+.movie-title,
+.icon {
+  color: red;
+  font-size: 20px;
+  padding: 15px 0;
+}
+
+.icon {
+  padding: 0;
+}
+
+.icon:hover {
+  cursor: pointer;
 }
 </style>
--- src/views/WatchList.vue
+++ src/views/WatchList.vue
@@ -1,14 +1,16 @@
 <template>
   <HeaderBar />
   <p>This is your watchlist</p>
+  <Movies :watchListPageBool="true" />
 </template>
 
 <script>
 import HeaderBar from '../components/HeaderBar.vue';
+import Movies from '../components/Movies.vue';
 
 export default {
   name: 'WatchList',
-  components: { HeaderBar },
+  components: { HeaderBar, Movies },
 };
 </script>
 
