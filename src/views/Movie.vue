03dbff011905185c09ad599b724493462132d07e
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-04 20:19:30 +0200

    added Movie component
    Component to view the details of individual movie

---
+++ src/views/Movie.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Movie item view page</p>
+  <p>{{ id }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Movie',
+  props: ['id'],
+  //   data() {
+  //     return {
+  //       id: this.$route.params.id,
+  //     };
+  //   },
+};
+</script>
+
+<style></style>
