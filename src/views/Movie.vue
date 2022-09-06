f189bf23e27e25f2d6d2e9bd7a170cb8fcb696c5
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-06 22:01:35 +0200

    display details on movie page component

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -1,17 +1,36 @@
 <template>
+  <HeaderBar />
   <p>Movie item view page</p>
-  <p>{{ id }}</p>
+  <p>{{ currMovie.name }}</p>
+  <img :src="currMovie.thumbnail" alt="" width="250" />
+  <ul>
+    <li v-for="(type, index) in currMovie.genre" :key="index">
+      {{ type }}
+    </li>
+  </ul>
+  <p>{{ currMovie.movieDescription }}</p>
 </template>
 
 <script>
+import HeaderBar from '../components/HeaderBar.vue';
+
 export default {
   name: 'Movie',
+  components: {
+    HeaderBar,
+  },
   props: ['id'],
-  //   data() {
-  //     return {
-  //       id: this.$route.params.id,
-  //     };
-  //   },
+  data() {
+    return {
+      movieId: this.$route.params.id,
+      currMovie: {},
+    };
+  },
+  created() {
+    JSON.parse(localStorage.getItem('movies')).forEach((element) => {
+      if (element.id === this.movieId) this.currMovie = element;
+    });
+  },
 };
 </script>
 
