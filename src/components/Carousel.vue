18263d4e41a1dc2713e85b5dc00e5cb91824dff1
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-06 22:01:58 +0200

    added carousel component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="carousel">
+    <p>This is the carousel area</p>
+  </div>
+</template>
+
+<script>
+import * as movies from '../mocks/data.json';
+
+export default {
+  name: 'Carousel',
+  props: ['movies'],
+  data() {
+    return {
+      moviesArr: [],
+    };
+  },
+  computed: {
+    moviesComingSoon() {
+      return this.moviesArr.filter((movie) => movie.comingSoon);
+    },
+  },
+  mounted() {
+    this.moviesArr = movies.movies;
+  },
+};
+</script>
+
+<style scoped>
+.carousel {
+  background: lightblue;
+  height: 425px;
+}
+</style>
