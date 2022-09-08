2e357d5840e9f99b2ad47f828ac67df33fdca02a
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-08 07:08:58 +0200

    logic for different logo sizes between pages

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="logo-wrapper">
     <router-link to="/home">
-      <img src="../assets/logo.png" alt="netflix-logo" class="logo" />
+      <img src="../assets/logo.png" alt="netflix-logo" class="logo" :class="{ bigLogo: bigLogo }" />
     </router-link>
   </div>
 </template>
@@ -9,6 +9,15 @@
 <script>
 export default {
   name: 'Logo',
+  props: ['biggerLogo'],
+  data() {
+    return {
+      bigLogo: false,
+    };
+  },
+  mounted() {
+    this.bigLogo = this.$props.biggerLogo;
+  },
 };
 </script>
 
@@ -21,8 +30,12 @@ export default {
 .logo {
   display: block;
   max-width: 140px;
-  /* background-color: rgba(0, 0, 0, 0.8);
-  box-shadow: rgba(0, 0, 0, 0.6) 0px 19px 38px, rgba(0, 0, 0, 0.8) 0px 15px 12px; */
+  /* background-color: rgba(0, 0, 0, 0.8); */
+  /* box-shadow: rgba(0, 0, 0, 0.6) 0px 19px 38px, rgba(0, 0, 0, 0.8) 0px 15px 12px; */
+}
+
+.bigLogo {
+  max-width: 290px;
 }
 
 @media screen and (min-width: 1000px) {
