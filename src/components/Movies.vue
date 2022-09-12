6923fe6b8b17fa30ef58f32f3e7c10ef91e3eb8f
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-12 21:56:54 +0200

    updated search bar styles

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -6,7 +6,7 @@
     <MovieItem v-if="!watchListPage" v-for="movie in sortedAvailMovies" :key="movie.id" :item="movie" @updateMovieWatchListVal="addToWatchList" />
     <div class="input-wrapper" v-if="watchListPage">
       <label for="search"></label>
-      <input type="text" v-model="searchVal" maxlength="64" placeholder="search" />
+      <input type="text" v-model="searchVal" maxlength="64" placeholder="search your movies" class="search" />
     </div>
     <MovieItem :item="movie" @updateMovieWatchListVal="addToWatchList" v-if="watchListPage" v-for="movie in sortedWatchlistMovies" :key="movie.id" />
   </div>
@@ -47,7 +47,15 @@ export default {
     },
   },
   mounted() {
-    if (!this.moviesArr.length) this.moviesArr = mockData.movies;
+    if (!this.moviesArr.length) {
+      this.moviesArr = mockData.movies;
+
+      // if (this.$route.path !== '/watchlist') {
+      //   this.moviesArr.forEach((movie) => {
+      //     if (movie.watchList) movie.watchList = false;
+      //   });
+      // }
+    }
 
     this.currDate = new Date().toJSON().slice(0, 10);
 
@@ -105,6 +113,20 @@ export default {
   margin: 20px 0;
 }
 
+.input-wrapper {
+  margin: 20px 0;
+}
+
+.search {
+  border-radius: 15px;
+  width: 350px;
+  padding: 15px 25px;
+}
+
+.search:focus {
+  outline: none;
+}
+
 @media screen and (min-width: 700px) {
   .content-container {
     max-width: 600px;
@@ -114,6 +136,10 @@ export default {
   }
 
   .carousel-wrapper {
+    grid-column: 1 / -1;
+  }
+
+  .input-wrapper {
     grid-column: 1 / -1;
   }
 }
