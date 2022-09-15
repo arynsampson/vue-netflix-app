1dcae20452dfb82f1b3af1116f4fcd4e01a0a4b4
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-15 22:15:42 +0200

    search component

---
+++ src/components/Search.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-wrapper">
+    <label for="search"></label>
+    <input type="text" v-model="searchVal" maxlength="64" placeholder="search movies" @change="onChange" @keyup="onChange" class="search" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data() {
+    return {
+      searchVal: '',
+    };
+  },
+  methods: {
+    onChange() {
+      this.$emit('search', this.searchVal);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-wrapper {
+  margin: 20px 0;
+}
+
+.search {
+  border-radius: 15px;
+  width: 350px;
+  padding: 15px 25px;
+}
+
+.search:focus {
+  outline: none;
+}
+</style>
