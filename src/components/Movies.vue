21b7d9badf0424a8b70640a9e884964b4b0f21ca
Aryn Sampson <arynsampson0704@gmail.com>
2022-09-15 22:16:05 +0200

    movies page refactor

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="home-content content-container">
-    <div v-if="!watchListPage" class="carousel-wrapper">
-      <Carousel :movies="moviesArr" />
-    </div>
-    <MovieItem v-if="!watchListPage" v-for="movie in sortedAvailMovies" :key="movie.id" :item="movie" @updateMovieWatchListVal="addToWatchList" />
-    <div class="input-wrapper" v-if="watchListPage">
-      <label for="search"></label>
-      <input type="text" v-model="searchVal" maxlength="64" placeholder="search your movies" class="search" />
-    </div>
-    <MovieItem :item="movie" @updateMovieWatchListVal="addToWatchList" v-if="watchListPage" v-for="movie in sortedWatchlistMovies" :key="movie.id" />
+    <slot></slot>
   </div>
 </template>
 
@@ -19,112 +11,87 @@ import Carousel from './Carousel.vue';
 
 export default {
   name: 'Movies',
-  components: {
-    MovieItem,
-    Carousel,
-  },
-  props: ['watchListPageBool', 'search'],
-  data() {
-    return {
-      moviesArr: [],
-      watchListPage: false,
-      currDate: '',
-      searchVal: '',
-    };
-  },
-  methods: {
-    addToWatchList(item) {
-      item.watchList = !item.watchList;
-    },
+  // components: {
+  //   MovieItem,
+  //   Carousel,
+  // },
+  // props: ['watchListPageBool', 'search'],
+  // data() {
+  //   return {
+  //     moviesArr: [],
+  //     watchListPage: false,
+  //     currDate: '',
+  //     searchVal: '',
+  //   };
+  // },
+  // methods: {
+  //   addToWatchList(item) {
+  //     item.watchList = !item.watchList;
+  //   },
 
-    sortArr(arrToSort) {
-      arrToSort.sort(function (a, b) {
-        let movieA = a.name.toUpperCase();
-        let movieB = b.name.toUpperCase();
-        return movieA < movieB ? -1 : movieA > movieB ? 1 : 0;
-      });
-      return arrToSort;
-    },
-  },
-  mounted() {
-    if (!this.moviesArr.length) {
-      this.moviesArr = mockData.movies;
+  //   sortArr(arrToSort) {
+  //     arrToSort.sort(function (a, b) {
+  //       let movieA = a.name.toUpperCase();
+  //       let movieB = b.name.toUpperCase();
+  //       return movieA < movieB ? -1 : movieA > movieB ? 1 : 0;
+  //     });
+  //     return arrToSort;
+  //   },
+  // },
+  // mounted() {
+  //   if (!this.moviesArr.length) {
+  //     this.moviesArr = mockData.movies;
 
-      // if (this.$route.path !== '/watchlist') {
-      //   this.moviesArr.forEach((movie) => {
-      //     if (movie.watchList) movie.watchList = false;
-      //   });
-      // }
-    }
+  //     // if (this.$route.path !== '/watchlist') {
+  //     //   this.moviesArr.forEach((movie) => {
+  //     //     if (movie.watchList) movie.watchList = false;
+  //     //   });
+  //     // }
+  //   }
 
-    this.currDate = new Date().toJSON().slice(0, 10);
+  //   this.currDate = new Date().toJSON().slice(0, 10);
 
-    if (this.$props.watchListPageBool) {
-      this.watchListPage = true;
-    }
+  //   if (this.$props.watchListPageBool) {
+  //     this.watchListPage = true;
+  //   }
 
-    this.moviesArr.forEach((movie) => {
-      if (this.currDate >= movie.availDate) {
-        movie.comingSoon = false;
-      }
-    });
-  },
-  computed: {
-    moviesAvailNow() {
-      return this.moviesArr.filter((movie) => !movie.comingSoon);
-    },
-    sortedAvailMovies() {
-      return this.sortArr(this.moviesAvailNow);
-    },
-    searchWatchListMovies() {
-      return this.moviesArr.filter((movie) => movie.watchList && movie.name.toLowerCase().includes(this.searchVal));
-    },
-    sortedWatchlistMovies() {
-      return this.sortArr(this.searchWatchListMovies);
-    },
-  },
-  watch: {
-    moviesArr: {
-      handler() {
-        localStorage.setItem('movies', JSON.stringify(this.moviesArr));
-      },
-      deep: true,
-    },
-  },
+  //   this.moviesArr.forEach((movie) => {
+  //     if (this.currDate >= movie.availDate) {
+  //       movie.comingSoon = false;
+  //     }
+  //   });
+  // },
+  // computed: {
+  //   moviesAvailNow() {
+  //     return this.moviesArr.filter((movie) => !movie.comingSoon);
+  //   },
+  //   sortedAvailMovies() {
+  //     return this.sortArr(this.moviesAvailNow);
+  //   },
+  //   searchWatchListMovies() {
+  //     return this.moviesArr.filter((movie) => movie.watchList && movie.name.toLowerCase().includes(this.searchVal));
+  //   },
+  //   sortedWatchlistMovies() {
+  //     return this.sortArr(this.searchWatchListMovies);
+  //   },
+  // },
+  // watch: {
+  //   moviesArr: {
+  //     handler() {
+  //       localStorage.setItem('movies', JSON.stringify(this.moviesArr));
+  //     },
+  //     deep: true,
+  //   },
+  // },
 };
 </script>
 
 <style scoped>
 .content-container {
-  max-width: 450px;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 15px;
-}
-
-.home-content {
-  margin: 0 auto;
-  padding: 110px 0;
-}
-
-.carousel-wrapper {
-  width: 100%;
-  margin: 20px 0;
-}
-
-.input-wrapper {
-  margin: 20px 0;
-}
-
-.search {
-  border-radius: 15px;
-  width: 350px;
-  padding: 15px 25px;
-}
-
-.search:focus {
-  outline: none;
 }
 
 @media screen and (min-width: 700px) {
